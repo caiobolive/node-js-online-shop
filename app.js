17cfad4dcf4a44322acb6abd2ae3a77821c6d80e
caiobolive <caiobolive@gmail.com>
2022-07-26 13:06:50 -0300

    finalized deployment

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const adminRoutes = require('./routes/admin.routes');
 const cartRoutes = require('./routes/cart.routes');
 const ordersRoutes = require('./routes/orders.routes');
 
-let PORT = 3000;
+let port = 3000;
 
 if (process.env.PORT) {
   port = process.env.PORT;
