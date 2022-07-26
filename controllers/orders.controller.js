92d38a5fea1d97ba617f3a39b86b630a32b7537c
caiobolive <caiobolive@gmail.com>
2022-07-26 13:39:47 -0300

    finalized deployment

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -51,8 +51,8 @@ async function addOrder(req, res, next) {
       }
     }),
     mode: 'payment',
-    success_url: `http://localhost:3000/orders/success`,
-    cancel_url: `http://localhost:3000/orders/failure`,
+    success_url: `http://127.0.0.1:3000/orders/success`,
+    cancel_url: `http://127.0.0.1:3000/orders/failure`,
   });
 
   res.redirect(303, session.url);
